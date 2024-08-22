adafe8a4951e96ba5422aa1162feff78478ecf48
zkkv <zkkv@proton.me>
2024-08-22 03:45:15 +0200

    Extract output string formatting logic

--- src/lib.rs
+++ src/lib.rs
@@ -10,15 +10,8 @@ use std::{collections::LinkedList, error::Error, path::Path};
 
 pub fn run(tweet_id: &str, options: &Options) -> Result<(), Box<dyn Error>> {
 	let thread = unwrap(tweet_id)?;
-
-	let mut buffer = String::from("# ");
-
-	for tweet in thread.tweets().iter() {
-		buffer = format!("{}{}\n\n", buffer, &tweet.text);
-	}
-
-	write_to_file(&buffer, &options.output)?;
-
+	let formatted = format_thread(&thread, &options);
+	write_to_file(&formatted, &options.output)?;
 	Ok(())
 }
 
@@ -35,9 +28,18 @@ fn unwrap(tweet_id: &str) -> Result<Thread, reqwest::Error> {
 	Ok(Thread::new(tweets))
 }
 
-fn write_to_file(buffer: &str, destination: &Path) -> Result<(), std::io::Error> {
+fn format_thread(thread: &Thread, options: &Options) -> String {
+	let mut buffer = String::from("# ");
+
+	for tweet in thread.tweets().iter() {
+		buffer = format!("{}{}\n\n", buffer, &tweet.text);
+	}
+
+	buffer
+}
+
+fn write_to_file(string: &str, destination: &Path) -> Result<(), std::io::Error> {
 	let dest = destination.join("out.md");
-	println!("Destination: {:?}", dest);
-	std::fs::write(dest, buffer)?;
+	std::fs::write(dest, string)?;
 	Ok(())
 }
